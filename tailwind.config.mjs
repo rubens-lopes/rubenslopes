2a3ef50d1d43cb605d2d700b7f01861cbc225d9d
Rubens Lopes <github@rubenslop.es>
2024-04-15 18:35:25 -0400

    feat: create home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		container: {
+      center: true,
+			padding: `1rem`,
+    },
+		extend: {
+			colors: {
+				snow: `#fffafa`,
+				'metallic-red': `#aa2c2e`,
+				'eerie-black': `#1e1e1e`,
+				'cadmium-orange': `#f58634`,
+				'medium-vermilion': `#cd5531`,
+			},
+			fontFamily: {
+				figtree: `"Figtree", sans-serif`,
+			},
+		},
 	},
 	plugins: [],
 }
