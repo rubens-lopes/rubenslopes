f0895ce2841d794474246c4c8695d0253452f431
Rubens Lopes <github@rubenslop.es>
2024-04-25 22:29:39 -0400

    i18n: controls text

---
+++ src/i18n/controls.ts
@@ -0,0 +1,10 @@
+export const controls = {
+  en: {
+    'controls.email-label': `Contact`,
+    'controls.email-title': `Talk to me`,
+  },
+  'pt-br': {
+    'controls.email-label': `Contato`,
+    'controls.email-title': `Fale comigo`,
+  },
+}
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,13 +1,16 @@
 import { about } from './about'
 import { footer } from './footer'
+import { controls } from './controls'
 
 export const ui = {
   en: {
     ...about.en,
     ...footer.en,
+    ...controls.en,
   },
   'pt-br': {
     ...about['pt-br'],
     ...footer['pt-br'],
+    ...controls['pt-br'],
   },
 }
